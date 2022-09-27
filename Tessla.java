375f6515ebb928e777c8847f9624cd1035b598e9
Archana Jha <114074480+12ARCHANA12@users.noreply.github.com>
2022-09-27 12:51:27 +0530

    Add files via upload

---
+++ Tessla.java
@@ -0,0 +1,15 @@
+package com.src;
+ class Tessla extends Carr {
+	 void drive() {
+		 System.out.println("drive safely");
+	 
+	 }
+	 public static void main(String[] args) {
+		 //TODP auto generate method stub
+		 Tessla t= new Tessla();
+		 //object of class tessla
+     t.drive();
+     t.gear();
+	 }
+
+}
